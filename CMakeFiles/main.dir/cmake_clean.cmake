ea97283b7d0d33a96163f269cae4f9e92a8490e0
quwenjie <2382036672@qq.com>
2024-12-03 02:55:41 -0800

    final version of rangeproof and added plookup

---
+++ CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/kzg.cpp.o"
+  "CMakeFiles/main.dir/kzg.cpp.o.d"
+  "CMakeFiles/main.dir/plookup.cpp.o"
+  "CMakeFiles/main.dir/plookup.cpp.o.d"
+  "CMakeFiles/main.dir/subgroups.cpp.o"
+  "CMakeFiles/main.dir/subgroups.cpp.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
